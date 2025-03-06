89c607c6888782a6534859ce70abb58dd315470d
ROMLIANY <lianirom30@gmail.com>
2025-03-06 14:37:38 +0200

    Update2 main.tf

--- devops-project/terraform/main.tf
+++ devops-project/terraform/main.tf
@@ -1,25 +1,46 @@
-resource "google_container_cluster" "primary" {
-  name     = "devops-cluster"
-  location = "us-central1"
+provider "google" {
+  project = "windy-gearbox-451911-s2"  # עדכן את זה לשם הפרויקט שלך ב-GCP
+  region  = "us-east1"  # האזור שבו אתה עובד
+}
 
-  # Authentication configuration for the Kubernetes master
-  master_auth {
-    client_certificate_config {
-      issue_client_certificate = true
+resource "google_compute_instance" "vm_instance" {
+  name         = "flask-vm"
+  machine_type = "e2-medium"
+  zone         = "us-east1-b"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-2004-lts"
     }
   }
 
-  # Node pool configuration
-  node_pool {
-    name       = "primary-node-pool"
-    node_count = 3
-
-    node_config {
-      machine_type = "e2-medium"
-      disk_size_gb = 20
-    }
+  network_interface {
+    network = "default"
+    access_config {}  # מאפשר גישה חיצונית
   }
 
-  # Enable Shielded Nodes for added security
-  enable_shielded_nodes = true
+  metadata_startup_script = <<-EOT
+    #!/bin/bash
+    sudo apt update -y
+    sudo apt install -y docker.io
+    sudo systemctl start docker
+    sudo systemctl enable docker
+
+    # להריץ את ה-MySQL
+    sudo docker run -d --name mysql_db \
+      -e MYSQL_ROOT_PASSWORD=root \
+      -e MYSQL_DATABASE=flask_db \
+      -e MYSQL_USER=user \
+      -e MYSQL_PASSWORD=password \
+      -p 3306:3306 mysql:5.7
+
+    # להריץ את Flask
+    sudo docker run -d -p 5000:5000 --name flask_app \
+      --link mysql_db:mysql \
+      romliani/devops-project-flask:latest
+  EOT
+}
+
+output "vm_external_ip" {
+  value = google_compute_instance.vm_instance.network_interface.0.access_config.0.nat_ip
 }
